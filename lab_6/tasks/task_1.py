130b19ad7d3d1b332484b96933b2a9e752b0752a
AntoninaPater <antonina.m.pater@gmail.com>
2019-11-22 22:00:45 +0100

    lab 6

--- lab_6/tasks/task_1.py
+++ lab_6/tasks/task_1.py
@@ -33,11 +33,67 @@ UWAGA: Możliwe jest wykonanie tylko jednej opcji +1 pkt.
 Otrzymuje się wtedy 2 pkt.
 UWAGA 2: Wszystkie jednoski masy występują w przykładzie.
 """
+import itertools
+from collections import namedtuple
 from pathlib import Path
+import csv
 
 
 def select_animals(input_path, output_path, compressed=False):
-    pass
+    with open(input_path) as _file:
+        reader = csv.reader(_file, delimiter=',')
+
+        animal_list = []
+        genera = set()
+        genders = ["female", "male"]
+        header = next(reader)
+        print(header)
+        animal = namedtuple('animal', header)
+        for row in reader:
+            current_animal = animal(*row)
+            animal_list.append(current_animal)
+            genera.add(current_animal.genus)
+
+    chosen_ones = []
+    for gender, genus in (itertools.product(genders, genera)):
+        selected_one = min(animal_list, key=lambda a: (a.genus == genus,
+                                                            a.gender == gender,
+                                                            float(a.mass.split(' ')[0])))
+        chosen_ones.append(selected_one)
+
+    chosen_ones = sorted(chosen_ones, key=lambda a: (a.genus, a.name))
+
+    gender = {
+        'female': 'F',
+        'male': 'M',
+    }
+    unit = {
+        "kg": 1.,
+        "g": 10 ** -3,
+        "mg": 10 ** -6,
+        "Mg": 10 ** 3,
+    }
+    compressed_animal = namedtuple('compressed_animal', 'id gender mass')
+    chosen_compressed_animals = []
+    for one in chosen_ones:
+        tmp_gender = gender[one.gender]
+        tmp_mass, tmp_unit = one.mass.split(' ')
+        tmp_mass = float(tmp_mass) * unit[tmp_unit]
+        tmp_mass = '{:.3e}'.format(tmp_mass)
+        current_compressed = compressed_animal(one.id, tmp_gender, tmp_mass)
+        print(tmp_mass)
+        chosen_compressed_animals.append(current_compressed)
+
+    if not compressed:
+        with open(output_path, 'w', newline='') as file_:
+            writer = csv.writer(file_, delimiter=',')
+            writer.writerow(header)
+            writer.writerows(chosen_ones)
+    else:
+        with open(output_path, 'w', newline='') as file_:
+            writer = csv.writer(file_, delimiter='_')
+            writer.writerow(['uuid', 'gender', 'mass'])
+            writer.writerows(chosen_compressed_animals)
 
 
 if __name__ == '__main__':
--- lab_6/tasks/task_2.py
+++ lab_6/tasks/task_2.py
@@ -12,10 +12,26 @@ dwuelementową tuplę zawierającą liczbę poprawnych wierszy:
 import re
 
 
+def match_msg(pattern, msg):
+    return bool(re.fullmatch(pattern, msg))
+
+
 def check_animal_list(file_path):
-    pass
+    with open(file_path) as _file:
+        rows = _file.readlines()
+
+    f_count = 0
+    m_count = 0
+    f_pattern = r'^[a-fA-F\d]{8}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{12}_F_[\d]\.[\d]{3}e[\-\+][\d]{2}$'
+    m_pattern = r'^[a-fA-F\d]{8}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{12}_M_[\d]\.[\d]{3}e[\-\+][\d]{2}$'
+
+    for row in rows:
+        row = row.strip()
+        f_count += match_msg(f_pattern, row)
+        m_count += match_msg(m_pattern, row)
+    return f_count, m_count
 
 
 if __name__ == '__main__':
     assert check_animal_list('s_animals_sce.txt') == (2, 2)
-    assert check_animal_list('animals_sc_corrupted.txt') == (6, 0)
+    assert check_animal_list('animals_sc_corrupted.txt') == (5, 1)
