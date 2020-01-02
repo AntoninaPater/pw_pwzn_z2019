46a9bca1fb28450b17aa07d5087aa9bba322a527
AntoninaPater <antonina.m.pater@gmail.com>
2020-01-02 14:18:20 +0100

    lab 10 rozwiazania

--- lab_10/tasks/task_1.py
+++ lab_10/tasks/task_1.py
@@ -1,8 +1,31 @@
+from typing import Dict
+from urllib.parse import urljoin
 import requests
 
+API_URL = 'https://www.metaweather.com/api/'
 
-def get_cities_woeid(query: str, timeout: float = 5.):
-    pass
+
+def get_cities_woeid(query: str, timeout: float = 5.) -> Dict[str, int]:
+    location_url = urljoin(API_URL, 'location/search')
+    params = {"query": query}
+    try:
+        response = requests.get(location_url, params=params, timeout=timeout)
+    except requests.exceptions.Timeout as e:
+        print('timeout error', e)
+        raise requests.exceptions.Timeout
+    else:
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print('communication error', e)
+        cities_dict = {}
+        try:
+            cities_dict = {doc["title"]: doc["woeid"] for doc in response.json()}
+        except RuntimeError as e:
+            print('parsing error', e)
+
+    finally:
+        return cities_dict
 
 
 if __name__ == '__main__':
--- lab_10/tasks/task_2.py
+++ lab_10/tasks/task_2.py
@@ -1,6 +1,10 @@
+import csv
+import os
 import pathlib
 from typing import Optional, Union, List
+from urllib.parse import urljoin
 
+import requests
 
 API_URL = 'https://www.metaweather.com/api/'
 
@@ -10,7 +14,46 @@ def get_city_data(
         path: Optional[Union[str, pathlib.Path]] = None,
         timeout: float = 5.
 ) -> (str, List[str]):
-    pass
+    _path = pathlib.Path.cwd()
+    if isinstance(path, pathlib.PosixPath):
+        result_path = path
+    elif isinstance(path, str):
+        result_path = pathlib.PosixPath(path) / f'{woeid}_{year}_{month:02}'
+    elif path is None:
+        result_path = _path / f'{woeid}_{year}_{month:02}'
+    if not os.path.exists(os.path.dirname(str(result_path) + "/")):
+        os.makedirs(os.path.dirname(str(result_path) + "/"))
+
+    session = requests.session()
+    saved_files = []
+    for day in range(1, 32):
+        location_url = urljoin(API_URL, f'location/{woeid}/{year}/{month}/{day}')
+        try:
+            response = session.get(location_url, timeout=timeout)
+        except requests.exceptions.Timeout as e:
+            print('timeout error', e)
+            raise requests.exceptions.Timeout
+        else:
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                print('communication error',e)
+            try:
+                resp_json = response.json()
+            except RuntimeError as e:
+                print('parsing error', e)
+
+        if resp_json:
+            with open(result_path / f'{year}_{month:02}_{day:02}.csv', mode='w+') as _file:
+                writer = csv.writer(_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                row1 = True
+                for row in response.json():
+                    if row1:
+                        row1 = False
+                        writer.writerow(row.keys())
+                    writer.writerow(row.values())
+                saved_files.append(result_path / str(day))
+    return str(result_path), saved_files
 
 
 if __name__ == '__main__':
